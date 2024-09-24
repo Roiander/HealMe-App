5f485900a432c38efa898d48ff654cbca4b6c09c
DiegoAlonso04 <132526677+DiegoAlonso04@users.noreply.github.com>
2024-09-23 19:01:52 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Doctors from './components/Doctors/Doctors.component.vue';
+import Hospitals from './components/Hospitals/Hospitals.component.vue';
 import MainImage from './layouts/MainLayout.component.vue';
 
 const routes = [
     { path: '/', component: MainImage },
-    { path: '/doctores', component: Doctors }
+    { path: '/doctores', component: Doctors },
+    { path: '/hospitales', component: Hospitals }
 ];
 
 const router = createRouter({
